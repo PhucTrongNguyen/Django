f24f2ce8d039b21f17c0fc9ae122b21ccc4b904e
PhucTrongNguyen <100078103+PhucTrongNguyen@users.noreply.github.com>
2023-12-31 17:28:52 +0700

    Update views.py

--- userauths/views.py
+++ userauths/views.py
@@ -14,7 +14,7 @@ def register_view(request):
         if form.is_valid():
             new_user = form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f"Hey {username}, You account was created successfully")
+            messages.success(request, f"Này {username}, Tài khoản của bạn đã được tạo")
             new_user = authenticate(username=form.cleaned_data["email"],
                                     password=form.cleaned_data["password1"])
             login(request, new_user)
@@ -30,7 +30,7 @@ def register_view(request):
 
 def login_view(request):
     if request.user.is_authenticated:
-        messages.warning(request, f"You are already logged in")
+        messages.warning(request, f"Bạn đã đăng nhập")
         return redirect("core:index")
     
     if request.method == "POST":
@@ -42,10 +42,10 @@ def login_view(request):
 
             if user is not None:
                 login(request, user)
-                messages.success(request, "You are logged in")
+                messages.success(request, "Bạn đã đăng nhập")
                 return redirect("core:index")
             else:
-                messages.warning(request, "User does not exist, create an account.")
+                messages.warning(request, "Tài khoản không tồn tại, hãy tạo một tài khoản mới.")
             pass
         except:
             messages.warning(request, f"User with {email} does not exist")
@@ -56,5 +56,5 @@ def login_view(request):
 def logout_view(request):
     if request.user.is_authenticated:
         logout(request)
-        messages.success(request, "You logged out.")
+        messages.success(request, "Bạn đã đăng xuất.")
         return redirect("userauths:sign-in")
